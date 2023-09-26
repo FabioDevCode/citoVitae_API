e316b0f2e25c28c2290cb4546ac6748da826530e
FabioDevCode <0677384849@orange.fr>
2023-09-26 20:28:35 +0200

    [ADD - Feature] - get completed profil

--- controllers/profilController.js
+++ controllers/profilController.js
@@ -1,8 +1,13 @@
 const dayjs = require('dayjs');
 const profilModel = require('../models/profilModel');
+const experiencesModel = require('../models/experiencesModel');
+const competencesModel = require('../models/competencesModel');
+const diplomesModel = require('../models/diplomesModel');
+const projetsModel = require('../models/projetsModel');
+const linksModel = require('../models/linksModel');
 
 
-const update = async(req, res, next) => {
+const update = async(req, res) => {
     try {
         const { uid } = req.user;
         const objToUpdate = {};
@@ -13,7 +18,7 @@ const update = async(req, res, next) => {
             } else if(value.length) {
                 objToUpdate[key] = value
             }
-        }
+        };
 
         const porfil_update = await profilModel.updateOne(
             { uid: uid },
@@ -22,16 +27,62 @@ const update = async(req, res, next) => {
 
         if(!porfil_update) {
             throw new Error('error');
-        }
+        };
 
         res.status(200).json({msg: 'Le profil a bien été mis à jour.'});
     } catch (err) {
         console.error('profilController : ' + err);
         res.status(500).json({err});
     }
-}
+};
+
+const completed = async(req, res) => {
+    try {
+        const { uid } = req.user;
+
+        const profil = await profilModel.findOne(
+            { uid },
+            { uid: 0, __v: 0 }
+        );
+        const competences = await competencesModel.find(
+            { uid },
+            { uid: 0, __v: 0 }
+        );
+        const experiences = await experiencesModel.find(
+            { uid },
+            { uid: 0, __v: 0 }
+        )
+        const diplomes = await diplomesModel.find(
+            { uid },
+            { uid: 0, __v: 0 }
+        )
+        const links = await linksModel.find(
+            { uid },
+            { uid: 0, __v: 0 }
+        )
+        const projets = await projetsModel.find(
+            { uid },
+            { uid: 0, __v: 0 }
+        )
+
+        res.status(200).json({
+            data: {
+                profil,
+                experiences,
+                diplomes,
+                competences,
+                projets,
+                links
+            }
+        });
+    } catch (err) {
+        console.error('profilController : ' + err);
+        res.status(500).json({err});
+    }
+};
 
 
 module.exports = {
-    update
-}
+    update,
+    completed
+};
